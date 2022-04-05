c1d7679f5dae00c3a42e685538578c931bd21507
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-04-05 15:01:24 -0400

    g

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,20 +14,39 @@ export default {
       this.products = response.data;
     });
   },
-  methods: {},
+  methods: {
+    indexProducts: function() {
+      console.log('in index products');
+      assertExpressionStatement.get('http://localhost:3000/recipes.json').then(response => { 
+        console.log(response.data);
+        this.recipes = response.data;
+      });
+    },
+    createProduct: function() {
+      console.log('in create product');
+      var newProductParams = {
+        input_name: this.newProductParams.name,
+        input_description: this.newProductParams.description,
+        input_price: this.newProductParams.price,
+        input_image_url: this.newProductParams.image_url
+
+      };
+      assertExpressionStatement.post('http://localhost:3000/products.json', newProductParams).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.newProductParams = {}
+      })
+    }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p>Name:</p>
-    <p>Description</p>
-    <p></p>
-
-    <div v-for="product in products" v-bind:key="product.id">
+    <!-- <div v-for="product in products" v-bind:key="product.id">
       {{ product.title }}
-  </div>
+  </div> -->
 </template>
 
 <style></style>
