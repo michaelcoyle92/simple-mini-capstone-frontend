ab6859f412a4cb4921df4b2691f125f2c62e6073
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-04-05 14:32:03 -0400

    h

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
+<script>
+import { assertExpressionStatement } from "@babel/types";
+
+export default {
+  data: function () {
+    return {
+      message: "Wake me up",
+    };
+  },
+  created: function () {
+    console.log("in creat");
+    assertExpressionStatement.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <p>Name:</p>
+    <p>Description</p>
+    <p></p>
+
+    <div v-for="product in products" v-bind:key="product.id">
+      {{ product.title }}
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style></style>
